4544de92e121f354ec44704e74320da34c6f4746
baibhav3211 <baibhavmishra3211@gmail.com>
2020-10-31 19:36:42 +0530

    cache update

--- iCoder/settings.py
+++ iCoder/settings.py
@@ -88,7 +88,12 @@ DATABASES = {
     }
 }
 
-
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': '/var/tmp/django_cache',
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
